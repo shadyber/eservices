188da1dd8c325e7d7e2e78e056e140cb369ecb51
Shady <shady.ber9@gmail.com>
2022-10-30 08:33:04 -0700

    update reoute sitemanp
    update sitemap

--- routes/web.php
+++ routes/web.php
@@ -150,51 +150,5 @@ Route::get('/mail-test',function (){
 });
 
 
-Route::get('sitemap', function() {
-
-    // create new sitemap object
-    $sitemap = App::make('sitemap');
-
-    // set cache key (string), duration in minutes (Carbon|Datetime|int), turn on/off (boolean)
-    // by default cache is disabled
-    $sitemap->setCache('laravel.sitemap', 60);
-
-    // check if there is cached sitemap and build new only if is not
-    if (!$sitemap->isCached()) {
-        // add item to the sitemap (url, date, priority, freq)
-        $sitemap->add(URL::to('/'), '2012-08-25T20:10:00+02:00', '1.0', 'daily');
-
-
-        // get all posts from db
-        $posts = DB::table('blogs')->orderBy('created_at', 'desc')->get();
-
-        // add every post to the sitemap
-        foreach ($posts as $post) {
-            $sitemap->add(URL::to('/').'/blog/'.$post->slug, $post->updated_at, '1.0', 'daily');
-        }
-
-
-        // get all posts from db
-        $videos = DB::table('videos')->orderBy('created_at', 'desc')->get();
-
-        // add every post to the sitemap
-        foreach ($videos as $post) {
-            $sitemap->add(URL::to('/').'/video/'.$post->slug, $post->updated_at, '1.0', 'daily');
-        }
-
-        // get all posts from db
-        $images = DB::table('blogs')->orderBy('created_at', 'desc')->get();
-
-        // add every post to the sitemap
-        foreach ($images as $post) {
-            $sitemap->add($post->thumb, $post->updated_at, '1.0', 'daily');
-        }
-
-
-    }
-
-    // show your sitemap (options: 'xml' (default), 'html', 'txt', 'ror-rss', 'ror-rdf')
-    return $sitemap->render('txt');
-});
 
 
